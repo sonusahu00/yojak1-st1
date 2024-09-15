4be5bc2a50c06bc3690d9075cb4b22a3d7c6fce1
sonu sahu <sonusahu982003@gmail.com>
2024-09-15 22:49:29 +0530

    adding static page

---
+++ a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 100000007
+
+int in(){
+  return 3;
+}
+
+int main(){
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int l = in();
+  cout<<l<<endl;
+
+  return 0;
+
+}
